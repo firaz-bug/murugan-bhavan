ba2699492fdb04522dd0800cf614a88811a006bc
firaz-bug <75066251+firaz-bug@users.noreply.github.com>
2021-02-21 12:30:46 +0530

    Update urls.py

--- murugan_bhavan/urls.py
+++ murugan_bhavan/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
 
 from django.views.static import serve
 from django.conf.urls import url
@@ -8,6 +12,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('user.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
